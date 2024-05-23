e4946ab5ee67a2c6b5bd48906a1c60b38235c956
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2024-05-23 23:21:25 +0530

    redux setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,88 @@
-import { useState } from 'react'
+import './polyfills';
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { useMemo } from 'react'
 import NavBar from './components/NavBar'
+
+import { getDefaultConfig, RainbowKitProvider, darkTheme, createAuthenticationAdapter, RainbowKitAuthenticationProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SiweMessage } from 'siwe';
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+});
+
+const queryClient = new QueryClient();
 
 
 function App() {
 
-  return (
-    <>
-      <>
-        <div className="flex flex-col">
-          <NavBar />
+  const authAdapter = useMemo(() => {
+    return createAuthenticationAdapter({
+      getNonce: async () => {
+        const response = await fetch('/api/nonce');
+        return await response.text();
+      },
 
-        </div>
-      </>
-    </>
+      createMessage: ({ nonce, address, chainId }) => {
+        return new SiweMessage({
+          domain: window.location.host,
+          address,
+          statement: 'Sign in with Ethereum to the app.',
+          uri: window.location.origin,
+          version: '1',
+          chainId,
+          nonce,
+        });
+      },
+
+      getMessageBody: ({ message }) => {
+        return message.prepareMessage();
+      },
+
+      verify: async ({ message, signature }) => {
+        const verifyRes = await fetch('/api/verify', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ message, signature }),
+        });
+
+        return Boolean(verifyRes.ok);
+      },
+
+      signOut: async () => {
+        await fetch('/api/logout');
+      },
+    });
+  }, []);
+
+  return (
+
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitAuthenticationProvider adapter={authAdapter} status='unauthenticated'>
+          <RainbowKitProvider theme={darkTheme({
+            accentColor: '#7b3fe4',
+            accentColorForeground: 'black',
+            borderRadius: 'small',
+            fontStack: 'system',
+            overlayBlur: 'small',
+          })}
+          >
+            <div className="flex flex-col">
+              <NavBar />
+
+            </div>
+          </RainbowKitProvider>
+        </RainbowKitAuthenticationProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+
+
   )
 }
 
--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { signIn, useSession } from "next-auth/react";
+import { useAccount, useSignMessage } from "wagmi";
+import { useRouter } from "next/router";
+import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
 
 interface WalletConnectButtonProps {
     // Define prop types here
 }
 
 const WalletConnectButton: React.FC<WalletConnectButtonProps> = () => {
-    // Component logic using props
+
+    // const { isConnected, address, chain } = useAccount();
+    // const { status } = useSession();
+    // const { signMessageAsync } = useSignMessage();
+    // const { push } = useRouter();
+    // const { requestChallengeAsync } = useAuthRequestChallengeEvm();
+
+    // useEffect(() => {
+    //     const handleAuth = async () => {
+    //         const { message } = await requestChallengeAsync({
+    //             address: address,
+    //             chainId: chain.id,
+    //         });
+
+    //         const signature = await signMessageAsync({ message });
+
+    //         // redirect user after success authentication to '/user' page
+    //         const { url } = await signIn("moralis-auth", {
+    //             message,
+    //             signature,
+    //             redirect: false,
+    //             callbackUrl: "/user",
+    //         });
+    //         /**
+    //          * instead of using signIn(..., redirect: "/user")
+    //          * we get the url from callback and push it to the router to avoid page refreshing
+    //          */
+    //         push(url);
+    //     };
+    //     if (status === "unauthenticated" && isConnected) {
+    //         handleAuth();
+    //     }
+    // }, [status, isConnected]);
+
     return (
         <div className='block pt-4'>
             <ConnectButton.Custom>

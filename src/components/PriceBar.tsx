03d8b4b9971b5ad5b8629ab1457bfc975da103ad
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2024-05-28 15:27:36 +0530

    static side bar done

--- src/components/PriceBar.tsx
+++ src/components/PriceBar.tsx
@@ -12,13 +12,36 @@ const PriceBar: React.FC<PriceBarProps> = () => {
     // Component logic using props
     return (
         <div className=' text-white flex justify-between items-center border-b border-[#4B4B4B]'>
-            <div className=' flex gap-4'>
-                <div className='flex gap-4 items-center hover:bg-gray-700 p-5'>
+            <div className=' flex gap-4  h-[7vh] py-3 items-center'>
+                <div className='flex gap-4 items-center pl-5'>
                     <img src={ethlogo} alt="" />
                     <h1 className=' font-medium text-xl'>ETH/USDC</h1>
                     <ChevronDownIcon className='w-5 h-5' />
                 </div>
-                <Separator orientation="vertical" />
+                <Separator className="h-full w-[1px] bg-[#4B4B4B] dark:bg-gray-700" />
+                <h1 className='text-green-500 text-xl font-bold'>3,751.6</h1>
+                <Separator className="h-full w-[1px] bg-[#4B4B4B] dark:bg-gray-700" />
+                <div className=' flex flex-col items-start'>
+                    <h1 className=' text-[#4B4B4B] font-normal text-sm'>Index Price</h1>
+                    <h1 className=''>3,751.6</h1>
+                </div>
+                <Separator className="h-full w-[1px] bg-[#4B4B4B] dark:bg-gray-700" />
+                <div className=' flex flex-col items-start'>
+                    <h1 className=' text-[#4B4B4B] font-normal text-sm'>Oracle Price</h1>
+                    <h1 className=''>3,751.5</h1>
+                </div>
+                <Separator className="h-full w-[1px] bg-[#4B4B4B] dark:bg-gray-700" />
+                <div className=' flex flex-col items-start'>
+                    <h1 className=' text-[#4B4B4B] font-normal text-sm'>24h Change</h1>
+                    <h1 className='text-green-500'>0.09 (0.02%)</h1>
+                </div>
+                <Separator className="h-full w-[1px] bg-[#4B4B4B] dark:bg-gray-700" />
+                <div className=' flex flex-col items-start'>
+                    <h1 className=' text-[#4B4B4B] font-normal text-sm'>24H Volume</h1>
+                    <h1 className=''>150,951.669</h1>
+                </div>
+
+                {/* </div> */}
             </div>
             <Button variant="ghost" color=''>Customize</Button>
         </div>
